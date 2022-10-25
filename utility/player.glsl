569aa8aaf74201de6ca884e721010e8a4b16a193
Bohdan <bodya.ru02@gmail.com>
2022-10-25 20:24:16 +0300

    But you didn't have to cut me off...

--- utility/player.glsl
+++ utility/player.glsl
@@ -1,7 +1,5 @@
 #include "render.glsl"
 
-#iChannel3 "file://rotate.glsl"
-
 
 float createPlayer(vec3 point, vec3 originPos, vec3 controls, vec3 offset) {
     Piramid body = Piramid(vec3(0., 0., 0.) + originPos + offset, 1.5);
