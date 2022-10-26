4e466db5937d0243ef4b6735589cdebc655ffa80
Adeon18 <ostaptrush18@gmail.com>
2022-10-26 15:33:34 +0300

    Added docs to player creation and rotation

--- utility/player.glsl
+++ utility/player.glsl
@@ -2,21 +2,25 @@
 
 #iChannel2 "file://rotate.glsl"
 
+
+/*
+ * Create the player object(Piramid) and handle input transformation and rotation
+*/
 float createPlayer(vec3 point, vec3 originPos, vec3 controls, vec3 offset) {
 
+    /// Get Rotation radians and matrix
     float rotationRad = texelFetch(iChannel2, ivec2(0, 0), 0).x;
     mat2 rotationMat = Rotate(rotationRad);
-    vec3 newOffset = vec3(0., offset.y, 0.);
 
+    /// Player body
     Piramid body = Piramid(vec3(0., 0., 0.) + originPos, 1.5);
 
-
+    /// Handle body rotation and body movement
     vec3 bodyPos = point - body.pos;
-
     bodyPos -= offset;
     bodyPos.xy *= rotationMat;
 
-
+    /// Flatten the piramid
     bodyPos *= vec3(1., 1., 2.);
 
     return sdPyramid(bodyPos, body.height) / 2.5;
--- utility/rotate.glsl
+++ utility/rotate.glsl
@@ -3,7 +3,11 @@
 #iChannel0 "self"
 #iChannel1 "file://input.glsl"
 
-
+/*
+ * Save the rotation radians so that the ship reembers it's previous
+ * rotation.
+ * ! There are 3 more variables to use in case needed :)
+*/
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     float rotationRad = texelFetch(iChannel0, ivec2(0, 0), 0).x;
