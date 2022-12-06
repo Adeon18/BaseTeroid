ed13ca59197db0e096fdf6ee2ae37ad5916dedd8
michael-2956 <work.michael.2956@gmail.com>
2022-12-06 20:36:09 +0200

    black hole

--- main.glsl
+++ main.glsl
@@ -7,6 +7,14 @@
 #iChannel0 "file://utility/data_channel.glsl"
 #iChannel1 "file://img/game_over.png"
 
+const vec3 BH_pos = vec3(0., 0., -10.);  // BH position
+const float BH_R = 0.5;  // event horizon radius
+/// how much acceleration a black hole exerts on a light particle
+vec3 blackHoleNullParticleAccl(vec3 p) {
+    vec3 r = (p - BH_pos);
+    return -1.5 * BH_R * r / pow(length(r), 5.0);
+}
+
 /* Get minimal distance to each object, objects are generated here for now */
 vec4 getColAndDist(vec3 point) {
     // vec3 bp = vec3(5., 0., 0.);
@@ -19,6 +27,9 @@ vec4 getColAndDist(vec3 point) {
 
     /// Use respective colors for respective objects
     vec4 colDist = minSd(vec4(PLAYER_COLOR, distPiramid), vec4(ASTEROID_COLOR, distAsteroids));
+
+    /// render event horizon
+    colDist = minSd(vec4(BH_pos, length(point) - BH_R), colDist);
 
     return colDist;
 }
@@ -57,14 +68,6 @@ vec3 getNormal(vec3 point) {
 //     return distToOrigin;
 // }
 
-const vec3 BH_pos = vec3(0., 0., 0.);  // BH position
-const float BH_R = 0.5;  // event horizon radius
-/// how much acceleration a black hole exerts on a light particle
-vec3 blackHoleNullParticleAccl(vec3 p) {
-    vec3 r = (p - BH_pos);
-    return -1.5 * BH_R * r / pow(length(r), 5.0);
-}
-
 /// current_position is the ray origin
 /// ray_velocity should be normalized
 /// color is the resulting color
@@ -72,48 +75,57 @@ const float dt = 0.15;
 vec4 blackHoleRender(vec3 current_position, vec3 ray_velocity, inout vec3 color) {
     // 4th coord is whether we found a point or diverged
     vec4 currentLocationAndMode = vec4(0.);
-    for (int i = 0; i < MAX_STEPS; ++i) {
+    for (int i = 0; i < MAX_STEPS * 10; ++i) {
         vec4 colAndDist = getColAndDist(current_position);
 
         ray_velocity += blackHoleNullParticleAccl(current_position) * dt;
 
         float vlen = length(ray_velocity);
-        // if (vlen > colAndDist.w) {
-        //     ray_velocity *= colAndDist.w / vlen;
-        // }
+        if (vlen > colAndDist.w) {
+            ray_velocity *= colAndDist.w / vlen;
+        }
 
         current_position += ray_velocity * dt;
 
         // check that we got below event horizon
         if (distance(BH_pos, current_position) < BH_R) {
-            currentLocationAndMode.xyzw = vec4(vec3(0.), 1.);
+            color = vec3(0.);
+            currentLocationAndMode.xyzw = vec4(current_position, 1.);
+            break;
         }
 
         // check that we landed on a point
-        if (abs(colAndDist.w) < vlen) {
+        if (abs(colAndDist.w) < SURF_DIST) {
             color = colAndDist.rgb;
             currentLocationAndMode = vec4(current_position, 1.);
             break;
         }
     }
-    if (currentLocationAndMode.w == 0.) {
-        ray_velocity = normalize(ray_velocity);
-        float distToOrigin = 0.;
-        for (int i = 0; i < MAX_STEPS; ++i) {
-            vec3 currentLocation = current_position + distToOrigin * ray_velocity;
-            /// rgb + w as length
-            vec4 colAndDist = getColAndDist(currentLocation);
+    // if (currentLocationAndMode.w == 0.) {
+    //     ray_velocity = normalize(ray_velocity);
+    //     float distToOrigin = 0.;
+    //     for (int i = 0; i < MAX_STEPS; ++i) {
+    //         vec3 currentLocation = current_position + distToOrigin * ray_velocity;
+    //         /// rgb + w as length
+    //         vec4 colAndDist = getColAndDist(currentLocation);
 
-            distToOrigin += colAndDist.w;
+    //         distToOrigin += colAndDist.w;
 
-            if (distToOrigin > MAX_DIST) break;
-            if (abs(colAndDist.w) < SURF_DIST) {
-                color = colAndDist.rgb;
-                currentLocationAndMode = vec4(currentLocation, 1.);
-                break;
-            }
-        }
-    }
+    //         // check that we got below event horizon
+    //         if (distance(BH_pos, currentLocation) < BH_R) {
+    //             color = vec3(0.);
+    //             currentLocationAndMode.xyzw = vec4(current_position, 1.);
+    //             break;
+    //         }
+
+    //         if (distToOrigin > MAX_DIST) break;
+    //         if (abs(colAndDist.w) < SURF_DIST) {
+    //             color = colAndDist.rgb;
+    //             currentLocationAndMode = vec4(currentLocation, 1.);
+    //             break;
+    //         }
+    //     }
+    // }
     return currentLocationAndMode;
 }
 
