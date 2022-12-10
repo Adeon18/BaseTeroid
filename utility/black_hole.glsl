6fd22b7b49c1c660121facee90e32936ec9728ef
michael-2956 <work.michael.2956@gmail.com>
2022-12-10 16:35:01 +0200

    60 fps

---
+++ utility/black_hole.glsl
@@ -0,0 +1,15 @@
+#ifndef BLACK_HOLE_GLSL
+#define BLACK_HOLE_GLSL
+
+#include "utility/common.glsl"
+
+const vec3 BH_pos = vec3(-.5, 1.5, -16.);  // BH position
+const float BH_R = 0.5;                  // event horizon radius
+/// how much acceleration a black hole exerts on a light particle
+vec3 blackHoleNullParticleAccl(vec3 p) {
+    vec3 r = (p - BH_pos);
+    return -1.5 * BH_R * r / pow(length(r), 5.0);
+}
+const float dt = 0.15;
+
+#endif  // BLACK_HOLE_GLSL
